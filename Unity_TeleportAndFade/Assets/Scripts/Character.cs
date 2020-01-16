7af9c8509095bb18c51d42b96caead69c764d122
matt2133leo <minghanlu@joybomb.net>
2020-01-16 17:01:18 +0800

    回家作業_W10

--- Unity_TeleportAndFade/Assets/Scripts/Character.cs
+++ Unity_TeleportAndFade/Assets/Scripts/Character.cs
@@ -1,26 +1,47 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Character : MonoBehaviour
 {
-   
+    [Header("透明數值"), Range(0, 255)]
+    public float alpha = 255;
+    [Header("淡入淡出速度"), Range(0f, 1f)]
+    public float speed = 0.1f;
 
+    private Image colorimage;
 
+    private void Start()
+    {
+        colorimage = GetComponent<Image>();
+        colorimage.color = new Color(255, 255, 255, alpha);
+        enterscene();
+    }
 
     private void OnTriggerEnter(Collider collider)
     {
         StartCoroutine(LoadScene());
     }
     
+
+
+    private void enterscene()
+    {
+        float t = 0;
+        for (float i = 0; i < 5; i+=Time.deltaTime)
+        {
+            t += speed;
+            alpha = Mathf.Lerp(255, 0, t*Time.deltaTime);
+        }
+
+    }
+
+
     private IEnumerator LoadScene()
     {
-        for (int i = 0; i < 10; i +=(int)Time.deltaTime)
-        {
-            SceneManager.LoadScene(1);
-        }
-        
-        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadSceneAsync(1);
+        yield return new WaitForSeconds(0.01f);
     }
 
 }
