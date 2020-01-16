a3d9806c916a0689754e4c192d38a3a68efe68cc
matt2133leo <minghanlu@joybomb.net>
2020-01-16 13:39:03 +0800

    回家作業_W10

---
+++ Unity_TeleportAndFade/Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+   
+
+
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        StartCoroutine(LoadScene());
+    }
+    
+    private IEnumerator LoadScene()
+    {
+        for (int i = 0; i < 10; i +=(int)Time.deltaTime)
+        {
+            SceneManager.LoadScene(1);
+        }
+        
+        yield return new WaitForSeconds(0.5f);
+    }
+
+}
